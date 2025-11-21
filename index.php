732a5ae2bd9e6708288dd626359b67d7b26d7db3
Manuel Cristobal <mcristobaln@outlook.com>
2025-11-21 17:01:38 -0500

    feat:add

--- index.php
+++ index.php
@@ -12,28 +12,11 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   	<div class="login-logo">
-  		<b>Sistema de Votaciones</b>
+  		<b>Se acabo el tiempo</b>
   	</div>
   
   	<div class="login-box-body">
-    	<p class="login-box-msg">Inicie Sesion Usuarios</p>
-
-    	<form action="login.php" method="POST">
-      		<div class="form-group has-feedback">
-        		<input type="text" class="form-control" name="dni" placeholder="Dni" required>
-        		<span class="glyphicon glyphicon-user form-control-feedback"></span>
-      		</div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="password" placeholder="Contraseña" required>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-          </div>
-      		<div class="row">
-    			<div class="col-xs-4">
-          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Ingresar</button>
-				</div>
-				<a href="voters.php" target="_blank">Registrate para Votar, por favor</a>
-      		</div>
-    	</form>
+    	<p class="login-box-msg">Ya no puede inciar sesion</p>
   	</div>
   	<?php
   		if(isset($_SESSION['error'])){
